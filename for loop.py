f70670b1c80e163eb464029011841409a48c896f
Gary stewart <gary.stewart@obsessivetech.co.uk>
2016-09-21 21:44:08 +0100

    Various excercise files from MITX EDX 6.00.1

---
+++ for loop.py
@@ -0,0 +1,5 @@
+end = 12
+total = 0
+for next in range(1, end+1):
+    total += next
+print(total)
---
+++ vowelcounter.py
@@ -0,0 +1,26 @@
+s = 'azcbobobegghakl'
+maxSub, currentSub, previousChar = '', '', ''
+for char in s:
+    if char >= previousChar:
+        currentSub = currentSub + char
+        if len(currentSub) > len(maxSub):
+            maxSub = currentSub
+    else: currentSub = char
+    previousChar = char
+print maxSub
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
