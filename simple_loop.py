c7bcf1da11251caf0e90d22a22d750c658e04362
Gary stewart <gary.stewart@obsessivetech.co.uk>
2016-09-21 22:09:45 +0100

    Various excercise files from MITX EDX 6.00.1

---
+++ simple_loop.py
@@ -0,0 +1,11 @@
+s = "hijklmnopqrstuvwxyz"
+for index in range(len(s)):
+    if __name__ == '__main__':
+        if s[index] == 'i' or s[index] == 'u':
+            print("There is an i or u")
+
+# or you could try this (comment out top one to run bottom one without an error :)
+
+# for char in s:
+#     if char == 'i' or char == 'u':
+#         print("There is an i or u")
