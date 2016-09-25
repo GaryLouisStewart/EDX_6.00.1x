c225b65be2f9290f4d0b75eb4ad9ff423dd5b2d2
Gary stewart <gary.stewart@obsessivetech.co.uk>
2016-09-25 20:01:24 +0100

    converting decimal integers to binary #1

---
+++ fractions.py
@@ -0,0 +1,16 @@
+num = int (input("Please enter your number here: "))
+
+if num < 0:
+    isNeg = True
+    num = abs(num)
+else:
+    isNeg = False
+result = ''
+if num == 0:
+    result = '0'
+while num > 0:
+    result = str(num%2) + result
+    num = num//2
+if isNeg:
+    result = '_' + result
+print(result)
